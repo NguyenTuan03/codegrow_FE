6feff4c8c36f9fed0bf55a0b78eeeaf8bcb484f7
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-04-11 14:14:34 +0700

    add header

--- src/lib/components/layout/header/Customerheader.tsx
+++ src/lib/components/layout/header/Customerheader.tsx
@@ -1,9 +1,31 @@
+import { Button } from '@/components/ui/button'
+import { CUSTOMER_HEADER } from '@/lib/enum/CustomerHeader'
+import Image from 'next/image'
 import React from 'react'
 
 
 const Customerheader = () => {
   return (
-    <div>Customerheader</div>
+    <div className='flex h-[80px]'>
+      <Image src="/Logo.png" alt="Logo" width={100} height={60} />
+      <div className='flex items-center justify-between w-full h-full px-4'>
+        <div className="flex flex-row w-full gap-5">
+          {
+            CUSTOMER_HEADER.map((item, index) => {
+              return (
+                <div key={index} className="flex flex-row items-center">
+                  <div className='font-semibold'>{item.name}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="flex flex-row items-center min-w-[180px]">
+          <Button className='me-3 border-[#767676]'>Sign in</Button>
+          <Button className='me-3'>Register</Button>          
+        </div>
+      </div>
+    </div>
   )
 }
 
