9a108fbed68ed9cdde3344db32efd8d1bd9da8c0
duyhoangto <hoangtdse170030@fpt.edu.vn>
2025-04-14 18:02:20 +0700

    header

--- src/lib/services/auth/VerifyGmail.ts
+++ src/lib/services/auth/VerifyGmail.ts
@@ -3,11 +3,11 @@ import httpRequest from './../HttpRequest';
 export const VerifyGmail = async (token: string) => {
     try {
         const res = await httpRequest.get('auth/verify', {
-            params: { token }, // Truyền token qua query parameter
+            params: { token },
         });
         return res.data;
     } catch (error) {
         console.error('Error verifying Gmail token:', error);
-        throw error; // Ném lỗi để xử lý ở nơi gọi hàm
+        throw error;
     }
 };
