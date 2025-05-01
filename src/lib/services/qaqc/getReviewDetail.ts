af87ac062e6e7f7a9e72119e413bd14ac66c3bd2
duyhoangto <hoangtdse170030@fpt.edu.vn>
2025-05-01 10:18:40 +0700

    fix lan 2 done

--- src/lib/services/qaqc/getReviewDetail.ts
+++ src/lib/services/qaqc/getReviewDetail.ts
@@ -1,15 +1,12 @@
-// @/lib/services/course/getReviews.ts
-
 import { get } from '@/lib/util/HttpRequest';
 
 export const GetReviewsByMentorId = async (mentorId: string, token: string) => {
     try {
-        const response = await get(`/classrooms/review/mentor?mentorId=${mentorId}`, {
+        const response = await get(`/classrooms/review/mentor/${mentorId}`, {
             headers: { Authorization: `Bearer ${token}` },
         });
-
         console.log('✅ Reviews API Response:', response);
-        return response.data; // Adjust based on your API response structure
+        return response.metadata;
     } catch (error) {
         console.error('❌ Error from GetReviewsByMentorId API:', error);
         throw error instanceof Error ? error : new Error('Failed to fetch reviews');
